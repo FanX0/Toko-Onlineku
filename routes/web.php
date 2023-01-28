0ebe846932e7ba0edabf5cd19c0f9fda60d3c379
FanX0 <faridazhari46@gmail.com>
2023-01-28 22:21:20 +0700

    Konversi Dashboard My Product Add Page

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::get('/register/success', [RegisteredUserController::class, 'success'])->n
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product');
-
+Route::get('/dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-product-create');
+Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product-details');
 Route::get('/dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-product-details');
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
