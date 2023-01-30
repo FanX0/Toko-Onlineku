7ae1ca92bfd0dd9b52c41f8c77e1f6f9bd239440
FanX0 <faridazhari46@gmail.com>
2023-01-30 20:23:20 +0700

    Add Halaman Statistik

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -18,7 +18,7 @@ Store Dashboard
                     <div class="card mb2">
                         <div class="card-body">
                             <div class="dashboard-card-title">Customer</div>
-                            <div class="dashboard-card-subtitle">15,209</div>
+                            <div class="dashboard-card-subtitle">{{ $customer }}</div>
                         </div>
                     </div>
                 </div>
@@ -26,7 +26,7 @@ Store Dashboard
                     <div class="card mb2">
                         <div class="card-body">
                             <div class="dashboard-card-title">Revenue</div>
-                            <div class="dashboard-card-subtitle">$15,209</div>
+                            <div class="dashboard-card-subtitle"> ${{ $revenue }}</div>
                         </div>
                     </div>
                 </div>
@@ -34,61 +34,12 @@ Store Dashboard
                     <div class="card mb2">
                         <div class="card-body">
                             <div class="dashboard-card-title">Transaction</div>
-                            <div class="dashboard-card-subtitle">22,409,399</div>
+                            <div class="dashboard-card-subtitle">{{ $customer }}</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row mt-3">
-                <div class="col-12 mt-2">
-                    <h5 class="mb-3">Recent Transaction</h5>
-                    <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-p1.png" alt="" />
-                                </div>
-                                <div class="col-md-4">Sirup Marjan</div>
-                                <div class="col-md-3">May Saroh</div>
-                                <div class="col-md-3">22-01-2023</div>
-                                <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-p2.png" alt="" />
-                                </div>
-                                <div class="col-md-4">Meja</div>
-                                <div class="col-md-3">Sapa dias</div>
-                                <div class="col-md-3">22-02-2023</div>
-                                <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-p3.png" alt="" />
-                                </div>
-                                <div class="col-md-4">Sofaa ku</div>
-                                <div class="col-md-3">joko Susanto</div>
-                                <div class="col-md-3">22-03-2023</div>
-                                <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
+
         </div>
     </div>
 </div>
