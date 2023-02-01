591433cd07eb6763017fd447ee0f65c6b303a22c
FanX0 <faridazhari46@gmail.com>
2023-02-02 02:19:59 +0700

    Integration Categories Section

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ use App\Http\Controllers\Admin\ProductGalleryController as AdminProductGalleryCo
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('/categories/{id}', [CategoryController::class, 'details'])->name('categories-detail');
 
 Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
 
