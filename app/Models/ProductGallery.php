1db651496ae5e4ab5f286d11feace5f16422064c
FanX0 <faridazhari46@gmail.com>
2023-02-01 20:57:06 +0700

    Product Gallery Model

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class ProductGallery extends Model
 {
     use HasFactory;
+
+     protected $fillable = [
+        'products_id',
+        'photos',
+    ];
+
+    protected $hidden = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
 }
