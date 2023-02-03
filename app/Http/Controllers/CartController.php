0f28029081a76d1de8b2dc992abb875d67aa7108
FanX0 <faridazhari46@gmail.com>
2023-02-03 20:14:58 +0700

    Cart Controller Modul Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
      public function index()
     {
-    return view('pages.cart');
+    $carts = Cart::with(['product','user'])->where('users_id', Auth::user()->id)->get();
+    return view('pages.cart',[
+        'carts' => $carts
+    ]);
     }
+
+    public function delete(Request $request, $id)
+    {
+        $cart = Cart::findOrFail($id);
+        $cart->delete();
+
+        return redirect()->route('cart');
+    }
+        
 
     public function success()
     {
