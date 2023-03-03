cfeeee535e5990121c35726bdd608c0fc919be40
FanX0 <faridazhari46@gmail.com>
2023-03-03 22:13:11 +0700

    Dashboard Page Integrasi View

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -18,7 +18,7 @@ Store Dashboard
                     <div class="card mb2">
                         <div class="card-body">
                             <div class="dashboard-card-title">Customer</div>
-                            <div class="dashboard-card-subtitle">15,209</div>
+                            <div class="dashboard-card-subtitle">{{ number_format($customer) }}</div>
                         </div>
                     </div>
                 </div>
@@ -26,7 +26,7 @@ Store Dashboard
                     <div class="card mb2">
                         <div class="card-body">
                             <div class="dashboard-card-title">Revenue</div>
-                            <div class="dashboard-card-subtitle">$15,209</div>
+                            <div class="dashboard-card-subtitle">{{ number_format($revenue) }}</div>
                         </div>
                     </div>
                 </div>
@@ -34,7 +34,7 @@ Store Dashboard
                     <div class="card mb2">
                         <div class="card-body">
                             <div class="dashboard-card-title">Transaction</div>
-                            <div class="dashboard-card-subtitle">22,409,399</div>
+                            <div class="dashboard-card-subtitle">{{ number_format($transactions_count) }}</div>
                         </div>
                     </div>
                 </div>
@@ -42,51 +42,31 @@ Store Dashboard
             <div class="row mt-3">
                 <div class="col-12 mt-2">
                     <h5 class="mb-3">Recent Transaction</h5>
-                    <a href="/dashboard-transactions-details.html" class="card card-list d-block">
+                    @foreach ($transactions_data as $transaction)
+                    <a href="{{ route('dashboard-product-transaction-details', $transaction->id) }}"
+                        class="card card-list d-block">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-p1.png" alt="" />
+                                    <img src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}"
+                                        class="w-50">
                                 </div>
-                                <div class="col-md-4">Sirup Marjan</div>
-                                <div class="col-md-3">May Saroh</div>
-                                <div class="col-md-3">22-01-2023</div>
+                                <div class="col-md-4">
+                                    {{ $transaction->product->name }}
+                                </div>
+                                <div class="col-md-3">
+                                    {{ $transaction->transaction->user->name }}
+                                </div>
+                                <div class="col-md-3">
+                                    {{ $transaction->created_at }}
+                                </div>
                                 <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
+                                    <img src="/images/dashboard-arrow-right.svg" alt="">
                                 </div>
                             </div>
                         </div>
                     </a>
-                    <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-p2.png" alt="" />
-                                </div>
-                                <div class="col-md-4">Meja</div>
-                                <div class="col-md-3">Sapa dias</div>
-                                <div class="col-md-3">22-02-2023</div>
-                                <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-p3.png" alt="" />
-                                </div>
-                                <div class="col-md-4">Sofaa ku</div>
-                                <div class="col-md-3">joko Susanto</div>
-                                <div class="col-md-3">22-03-2023</div>
-                                <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
+                    @endforeach
                 </div>
             </div>
         </div>
