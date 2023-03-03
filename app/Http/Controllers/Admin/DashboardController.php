c7e852201e9472245e189d418145f75cd6c7c68b
FanX0 <faridazhari46@gmail.com>
2023-03-03 21:59:09 +0700

    Dashboard Page Integrasi Controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\TransactuinDetail;
 
 use App\Models\User;
 use App\Models\Transaction;
